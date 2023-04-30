e1db85097edb499e8136bcc1ca18f82a373ee23a
SeRDic0705 <serdic0705@gmail.com>
2023-05-01 02:06:10 +0900

    Update PlayerController.cs
    Update script to prevent player moving outside of screen

--- Enigma/Assets/Scripts/PlayerController.cs
+++ Enigma/Assets/Scripts/PlayerController.cs
@@ -35,6 +35,13 @@ public class PlayerController : MonoBehaviour
             state = NewState(newVelocity);
             animator.SetInteger("State", (int)state);
         }
+
+        Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
+        if (pos.x < 0f) pos.x = 0f;
+        if (pos.x > 1f) pos.x = 1f;
+        if (pos.y < 0f) pos.y = 0f;
+        if (pos.y > 1f) pos.y = 1f;
+        transform.position = Camera.main.ViewportToWorldPoint(pos);
     }
 
     public State NewState(Vector2 vector)
